ddc004cbb9a72e906f3fb50c60e857ed738e1a0c
Arun-Filmy <130557833+Arun-Filmy@users.noreply.github.com>
2023-05-02 18:11:07 +0530

    Update StorageService.java

--- AWS s3/amazon-storage/src/main/java/com/storage/amazonstorage/service/StorageService.java
+++ AWS s3/amazon-storage/src/main/java/com/storage/amazonstorage/service/StorageService.java
@@ -1,44 +1,104 @@
 package com.storage.amazonstorage.service;
 
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.Bucket;
-import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.*;
+import com.amazonaws.util.IOUtils;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
+import java.io.*;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
-public class StorageService {
+@Transactional
+public class StorageService implements StorageServiceInterface{
+
+    @Value("${application.bucket.name}")
+    private String bucketName;
 
     @Autowired
     public AmazonS3 amazonS3;
 
+    @Override
     public String createBucket(String bucketName) {
-        Bucket bucket = amazonS3.createBucket(bucketName);
-
-        return bucket.getName();
+        if (!amazonS3.doesBucketExistV2(bucketName)) {
+            Bucket bucket = amazonS3.createBucket(bucketName);
+            return bucket.getName();
+        }else {
+            return bucketName;
+        }
     }
 
-    public String createFolder(String bucketName, String folderName) {
-        if (!amazonS3.doesBucketExistV2(bucketName)) {
-            throw new RuntimeException("Bucket does not exist: " + bucketName);
+
+
+//    public String createFolder(String bucketName, String folderName) {
+//        if (!amazonS3.doesBucketExistV2(bucketName)) {
+//            throw new RuntimeException("Bucket does not exist: " + bucketName);
+//        }
+//
+//        ObjectMetadata metadata = new ObjectMetadata();
+//        metadata.setContentLength(0);
+//
+//        InputStream emptyContent = new ByteArrayInputStream(new byte[0]);
+//
+//        PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName,
+//                folderName + "/", emptyContent, metadata);
+//
+//        amazonS3.putObject(putObjectRequest);
+//
+//        return folderName;
+//
+//    }
+
+    @Override
+    public String saveFile(MultipartFile file) {
+        String originalFilename = file.getOriginalFilename();
+        try {
+            File file1 = convertMultiPartFileToFile(file);
+            PutObjectResult putObjectResult = amazonS3.putObject(bucketName, originalFilename, file1);
+            return putObjectResult.getContentMd5();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
+    }
 
-        ObjectMetadata metadata = new ObjectMetadata();
-        metadata.setContentLength(0);
+    @Override
+    public byte[] downloadFile(String fileName) {
 
-        InputStream emptyContent = new ByteArrayInputStream(new byte[0]);
+        S3Object object = amazonS3.getObject(bucketName, fileName);
+        S3ObjectInputStream objectContent = object.getObjectContent();
+        try {
+            byte[] byteArray = IOUtils.toByteArray(objectContent);
+            return byteArray;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-        PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName,
-                folderName + "/", emptyContent, metadata);
+    @Override
+    public String deleteFile(String fileName) {
 
-        amazonS3.putObject(putObjectRequest);
+        amazonS3.deleteObject(bucketName, fileName);
+        return "File Deleted";
+    }
 
-        return folderName;
+    @Override
+    public List<String> listAllFiles() {
 
+        ListObjectsV2Result listObjectsV2Result = amazonS3.listObjectsV2(bucketName);
+        return listObjectsV2Result.getObjectSummaries().stream().map(S3ObjectSummary::getKey).collect(Collectors.toList());
+
+    }
+
+    private File convertMultiPartFileToFile(MultipartFile file) throws IOException{
+        File converFile = new File(file.getOriginalFilename());
+        FileOutputStream fos = new FileOutputStream(converFile);
+        fos.write(file.getBytes());
+        fos.close();
+        return converFile;
     }
 }
